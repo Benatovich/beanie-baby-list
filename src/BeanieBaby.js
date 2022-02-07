12c18a499cffe80318f2e310686ca12b8a9d3783
Benatovich <sambenatovich@gmail.com>
2022-02-07 18:30:54 -0500

    fixed a few styling issues and confirmed that all components work. moving on to stretch goals

--- src/BeanieBaby.js
+++ src/BeanieBaby.js
@@ -5,10 +5,10 @@ export default function BeanieBaby({ beanieBaby }) {
   return (
     // this should contain a react-router-dom Link to the detail page for this particular beanie baby.
     // it should also render the beanie baby's image and show the beanie baby's name
-    <Link key={beanieBaby.title} to={`/beanie-babies/${beanieBaby.id}`}>
-      <div className='beanie-baby'>
-        {beanieBaby.title}
+    <Link className='beanie-baby' key={beanieBaby.title} to={`/beanie-babies/${beanieBaby.id}`}>
+      <div className='baby'>
         <img src={beanieBaby.image} />
+        {beanieBaby.title}
       </div>
     </Link>
   );
