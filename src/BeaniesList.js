3b06b515a9072448f7c97f14b1a170634da6a23e
Benatovich <sambenatovich@gmail.com>
2022-02-07 17:08:58 -0500

    forgot to commit before lunch

--- src/BeaniesList.js
+++ src/BeaniesList.js
@@ -1,8 +1,17 @@
+import { Link } from 'react-router-dom';
 import BeanieBaby from './BeanieBaby.js';
 
 export default function BeaniesList({ beanieBabies }) {
   return (
     <div className='beanies'>
       {/* render out the beanie babies as a list */}
+      {
+        beanieBabies.map((item, i) => <Link key={item.title + i} to={`/beanie-babies/${item.id}`}>
+          <div className='baby'>
+            {item.title}
+            <img src={item.image} />
+          </div>
+        </Link>)
+      }
     </div>);
 }
